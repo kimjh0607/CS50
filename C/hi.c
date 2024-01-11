208a259f1a0a70af3f022b7ce12dec45447c86cd
WangChoBo <127456983+kimjh0607@users.noreply.github.com>
2024-01-11 20:00:20 +0900

    Update hi.c

--- C/hi.c
+++ C/hi.c
@@ -3,7 +3,16 @@
 
 int main(void)
 {
-    string s  = "HI!";
+    //string s  = "HI!";
     // printf("%s\n", s);
-    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]); // 72 73 33 0
+    //printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]); // 72 73 33 0
+    string words[2];
+    words[0] = "HI!";
+    words[1] = "BYE!";
+
+    // printf("%s\n", words[1]);
+    // printf("%s\n", words[2]);
+
+    printf("%c%c%c\n", words[0][0],words[0][1],words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
