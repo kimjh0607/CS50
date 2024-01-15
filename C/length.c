847bc3e98d55494d5187f2ba5a543526ecba9a05
WangChoBo <127456983+kimjh0607@users.noreply.github.com>
2024-01-15 23:48:48 +0900

    Create length.c

---
+++ C/length.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string name = get_string("Name : ");
+    int length = strlen(name);
+    printf("%i\n", length);
+}
+
+// int string_length(string s)
+// {
+//     int n = 0;
+//     while(s[n] != '\0')
+//     {
+//         n++;
+//     }
+//     return n;
+// }
